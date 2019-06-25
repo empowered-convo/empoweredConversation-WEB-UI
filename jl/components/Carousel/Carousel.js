ee86102d2ed9b141d5f15809c540bba3bad4a807
justinelai <justine.lai@gmail.com>
2019-06-25 18:36:42 -0400

    cleanup tablet margins

--- jl/components/Carousel/Carousel.js
+++ jl/components/Carousel/Carousel.js
@@ -1,30 +1,30 @@
-/*
+
 class Carousel {
     constructor(element) {
         this.element = element;
         this.left = this.element.querySelector(".left-button")
         this.right = this.element.querySelector(".right-button")
-        this.images = Array.from(this.element.querySelectorAll("img"))
+        this.cards = Array.from(this.element.querySelectorAll(".card"))
         this.currentIndex = 0;
-        this.images[this.currentIndex].style.display = "block"
+        this.cards[this.currentIndex].style.display = "block"
         this.left.addEventListener("click", () => this.prev())
         this.right.addEventListener("click", () => this.next())
     }
     prev() {   
-        this.images.forEach(item => item.style.display = "none");
+        this.cards.forEach(item => item.style.display = "none");
         this.currentIndex -= 1;
         if (this.currentIndex < 0) {
-            this.currentIndex = this.images.length - 1;
+            this.currentIndex = this.cards.length - 1;
         }
-        this.images[this.currentIndex].style.display = "block"
+        this.cards[this.currentIndex].style.display = "block"
     }
     next() {
-        this.images.forEach(item => item.style.display = "none");
+        this.cards.forEach(item => item.style.display = "none");
         this.currentIndex += 1;
-        if (this.currentIndex > this.images.length - 1 ) {
+        if (this.currentIndex > this.cards.length - 1 ) {
             this.currentIndex = 0;
         }
-        this.images[this.currentIndex].style.display = "block"
+        this.cards[this.currentIndex].style.display = "block"
     }
 }
 
