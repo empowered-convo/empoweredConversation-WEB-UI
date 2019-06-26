478852c8fa11f1d00970b0bc125217e6b1593178
justinelai <justine.lai@gmail.com>
2019-06-26 18:56:05 -0400

    add mouseleave element to dropdown component

--- jl/components/Dropdown/Dropdown.js
+++ jl/components/Dropdown/Dropdown.js
@@ -1,22 +1,22 @@
 class Dropdown {
   constructor(element) {
     
-    // Assign this.element to the dropdown element
     this.element = element;
     console.log(element + " is working!")
 
-    // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector(".dropdown-button");
     console.log(this.button)
     
-    // assign the reference to the ".dropdown-content" class found in the dropdown element
     this.content = this.element.querySelector(".dropdown-content");
     console.log(this.content)
 
-    // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', () => {
       this.toggleContent();
-    })
+    });
+
+    this.content.addEventListener('mouseleave', () => {
+      this.toggleContent();
+  })
   }
   toggleContent() {
     // Toggle the ".dropdown-hidden" class off and on
@@ -25,3 +25,4 @@ class Dropdown {
 }
 
 let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+
