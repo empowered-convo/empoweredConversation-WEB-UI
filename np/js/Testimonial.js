d249eb523c758493ea8827144acea44ac96dcbdd
Nikema Prophet <nikema@gmail.com>
2019-06-27 09:41:58 -0700

    Moved all js files to js folder

---
+++ np/js/Testimonial.js
@@ -0,0 +1,29 @@
+class Testimonial {
+  constructor(testimonial) {
+    this.testimonial = testimonial;
+    this.button = document.querySelector(".next-button");
+    this.reset();
+    this.index = 0;
+    this.limit = testimonials.length - 1;
+    this.button.addEventListener("click", () => this.showNextReview());
+  }
+  reset() {
+    testimonials[testimonials.length - 1].classList.remove("show");
+    this.index = 0;
+    testimonials[this.index].classList.add("show");
+  }
+
+  showNextReview() {
+    if (this.index < this.limit) {
+      testimonials[this.index].classList.toggle("show");
+      this.index++;
+      testimonials[this.index].classList.toggle("show");
+    } else {
+      this.reset();
+    }
+  }
+}
+
+const testimonials = document.querySelectorAll(".testimonial");
+
+testimonials.forEach(testimonial => new Testimonial(testimonial));
